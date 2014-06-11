26f3c30b189388b52c8cadeda4d49ddc223c14d4
framgianguyenbui <bui.binh.nguyen@framgia.com>
2014-06-11 08:14:18 +0700

    add staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/word_list'
+
+  get 'static_pages/categories'
+
+  get 'static_pages/lesson'
+
+  get 'static_pages/result'
+
+  get 'static_pages/user_show'
+
+  get 'static_pages/all_users'
+
+  get 'static_pages/lesson_list'
+
   resources :words
   resources :categories
   resources :users
